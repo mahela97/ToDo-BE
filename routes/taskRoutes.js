d6640900411ccf0feba0e8ce46faaa0b76092411
mahela97 <m.pradeepd@gmail.com>
2021-06-24 02:06:33 +0530

    gg

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const taskRouter = express.Router();
 
-taskRouter.get("/",(req,res)=>{res.send("task")});
+taskRouter.get("/",(req,res)=>{res.json({result: "gg"})});
+taskRouter.get("/x",(req,res)=>{res.send("tasks ")});
 
 module.exports = taskRouter;
