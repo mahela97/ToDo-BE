c437110a1177842a9086f483bb2e9f70e028782a
mahela97 <m.pradeepd@gmail.com>
2021-06-24 14:06:14 +0530

    login

--- controller/userController.js
+++ controller/userController.js
@@ -28,6 +28,47 @@ module.exports={
             res.status(error.code||409).send(error.message);
         }
 
+    },
+    loginUser:async(req,res)=>{
+        const body = req.body;
+        let userDetailsinDatabase;
+        try {
+            userDetailsinDatabase = await getRegistedUserByEmail(body.email);
+            if (userDetailsinDatabase && !userDetailsinDatabase.isDelete) {
+                const result = compareSync(
+                    body.password,
+                    userDetailsinDatabase.password
+                );
+                if (result) {
+                    userDetailsinDatabase.password = undefined;
+                    userDetailsinDatabase.user_photo = undefined;
+                    userDetailsinDatabase.userType= "User"
+                    const jsontoken = sign({ result: userDetailsinDatabase }, "qwe1234", {
+                        expiresIn: "1day",
+                    });
+                    return res.json({
+                        sucess: 1,
+                        message: "login Sucess",
+                        token: jsontoken,
+                    });
+                } else {
+                    return res.json({
+                        sucess: 0,
+                        message: "Password is invalid",
+                    });
+                }
+            } else {
+                return res.json({
+                    sucess: 0,
+                    message: "Invalid Email",
+                });
+            }
+        } catch (err) {
+            return res.json({
+                sucess: 0,
+                message: err,
+            });
+        }
     }
 
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const userRouter = express.Router();
 const userController = require("../controller/userController");
 
 userRouter.post("/register",userController.registerUser);
+userRouter.post("/login",userController.loginUser);
 
 module.exports = userRouter;
